bce5dd89aa6c2e97757d266d59747a79bccaeb2b
René Rafael <rene.rafael@groupsolver.com>
2024-02-18 21:36:06 +0100

    feat(d04): Part 1 done.

---
+++ d04/index.ts
@@ -0,0 +1,40 @@
+const inputFile = Bun.file("input");
+
+const input = await inputFile.text();
+const lines = input.split("\n").filter((l) => l.length !== 0);
+
+const p1 = () => {
+  const cards = lines.map((line) => {
+    const [, allNumbers] = line.split(": ");
+    const [winningNumbersAsString, myNumbersAsString] = allNumbers.split(" | ");
+
+    const winningNumbers = winningNumbersAsString
+      .split(" ")
+      .filter((n) => n.length !== 0)
+      .map(Number);
+
+    const myNumbers = myNumbersAsString
+      .split(" ")
+      .filter((n) => n.length !== 0)
+      .map(Number);
+
+    return { winningNumbers, myNumbers };
+  });
+
+  const points = cards.map(({ winningNumbers, myNumbers }) =>
+    myNumbers
+      .filter((mn) => winningNumbers.includes(mn))
+      .reduce((acc) => (acc === 0 ? acc + 1 : acc * 2), 0),
+  );
+
+  const res = points.reduce((acc, n) => acc + n);
+
+  console.log(res);
+};
+
+const p2 = () => {
+  console.log("Not implemented!");
+};
+
+p1();
+p2();
