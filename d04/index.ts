0bd51128674ee9959921c7b536092158f0925abe
René Rafael <rene.rafael@groupsolver.com>
2024-02-21 10:16:12 +0100

    feat(d04): Prepare for day 4 part 2 - This enters infinite loop for some reason

--- d04/index.ts
+++ d04/index.ts
@@ -4,7 +4,8 @@ const input = await inputFile.text();
 const lines = input.split("\n").filter((l) => l.length !== 0);
 
 const cards = lines.map((line) => {
-  const [, allNumbers] = line.split(": ");
+  const [prefixedIdCard, allNumbers] = line.split(": ");
+  const [, idCard] = prefixedIdCard.split(" ").filter((s) => s.length !== 0);
   const [winningNumbersAsString, myNumbersAsString] = allNumbers.split(" | ");
 
   const winningNumbers = winningNumbersAsString
@@ -17,7 +18,7 @@ const cards = lines.map((line) => {
     .filter((n) => n.length !== 0)
     .map(Number);
 
-  return { winningNumbers, myNumbers };
+  return { idCard: Number(idCard), winningNumbers, myNumbers };
 });
 
 const p1 = () => {
@@ -33,7 +34,42 @@ const p1 = () => {
 };
 
 const p2 = () => {
-  console.log("Not implemented!");
+  const p2Cards = cards.slice();
+
+  let currentIndex = 0;
+
+  // while (currentIndex < p2Cards.length) {
+  while (currentIndex < p2Cards.length) {
+    console.log("----------- START --------------");
+    const currentCard = p2Cards[currentIndex];
+
+    const winningNumbersLength = currentCard.myNumbers.filter((mn) =>
+      currentCard.winningNumbers.includes(mn),
+    ).length;
+
+    for (let i = currentIndex; i < currentIndex + winningNumbersLength; i++) {
+      const duplicateCard = p2Cards[i + 1];
+      p2Cards.push(duplicateCard);
+    }
+
+    p2Cards.sort((a, b) => {
+      if (a.idCard === b.idCard) {
+        return 0;
+      }
+
+      if (a.idCard < b.idCard) {
+        return -1;
+      }
+
+      return 1;
+    });
+
+    currentIndex++;
+    console.log("Length: ", p2Cards.length);
+    console.log("----------- END --------------");
+  }
+
+  console.log(p2Cards.length);
 };
 
 p1();
