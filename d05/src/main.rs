36f861ef64345e3af6a1ea7f286a8c161ef0b066
René Rafael <rene.rafael@groupsolver.com>
2024-03-31 19:13:46 +0200

    feat(d05): Finish part 1.

--- d05/src/main.rs
+++ d05/src/main.rs
@@ -26,29 +26,44 @@ fn read_lines(filename: &str) -> Vec<String> {
 
 #[derive(Debug, Clone)]
 struct FilterRange {
-    source: isize,
-    destination: isize,
-    length: isize,
+    start: isize,
     range: Range<isize>,
 }
 
 impl FilterRange {
-    pub fn new(source: isize, destination: isize, length: isize) -> Self {
-        let range = source..(source + length);
+    pub fn new(start: isize, length: isize) -> Self {
+        let range = start..(start + length);
 
-        return Self {
-            source,
-            destination,
-            length,
-            range,
-        };
+        return Self { start, range };
+    }
+
+    pub fn contains(&self, source: &isize) -> bool {
+        return self.range.contains(source);
     }
 }
 
-fn parse_lines(lines: &[String]) -> (Vec<isize>, HashMap<&str, Vec<FilterRange>>) {
+#[derive(Debug, Clone)]
+struct Map {
+    source: FilterRange,
+    destination: FilterRange,
+}
+
+impl Map {
+    pub fn get_destination(&self, source_value: isize) -> isize {
+        if self.source.contains(&source_value) {
+            let diff = source_value - self.source.start;
+
+            return self.destination.start + diff;
+        }
+
+        return source_value;
+    }
+}
+
+fn parse_lines(lines: &[String]) -> (Vec<isize>, Vec<Vec<Map>>) {
     let mut seeds = vec![];
     let mut map_names = vec![];
-    let mut maps: HashMap<&str, Vec<FilterRange>> = HashMap::new();
+    let mut filter_map_hash: HashMap<&str, Vec<Map>> = HashMap::new();
 
     for line in lines.iter() {
         if line.starts_with("seeds: ") {
@@ -74,25 +89,65 @@ fn parse_lines(lines: &[String]) -> (Vec<isize>, HashMap<&str, Vec<FilterRange>>
 
             let mut numbers = numbers.iter();
 
-            let filter_range = FilterRange::new(
-                *numbers.next().unwrap(),
-                *numbers.next().unwrap(),
-                *numbers.next().unwrap(),
-            );
+            let destination = *numbers.next().unwrap();
+            let source = *numbers.next().unwrap();
+            let length = *numbers.next().unwrap();
 
-            maps.entry(map_names.last().unwrap())
-                .and_modify(|value| value.push(filter_range.clone()))
-                .or_insert(vec![filter_range.clone()]);
+            let source_filter_range = FilterRange::new(source, length);
+            let destination_filter_range = FilterRange::new(destination, length);
+
+            let filter_map = Map {
+                source: source_filter_range,
+                destination: destination_filter_range,
+            };
+
+            filter_map_hash
+                .entry(map_names.last().unwrap())
+                .and_modify(|v| v.push(filter_map.clone()))
+                .or_insert(vec![filter_map.clone()]);
         }
     }
 
-    return (seeds, maps);
+    let mut ordered_filter_maps: Vec<Vec<Map>> = vec![];
+
+    for map_name in map_names {
+        let a = filter_map_hash.get(map_name.as_str()).unwrap().to_vec();
+
+        ordered_filter_maps.push(a);
+    }
+
+    return (seeds, ordered_filter_maps);
+}
+
+fn part_one(lines: &[String]) {
+    let (seeds, maps) = parse_lines(lines);
+
+    let location = seeds
+        .iter()
+        .map(|seed| {
+            maps.iter().fold(*seed, |acc, vec_map| {
+                vec_map
+                    .iter()
+                    .find(|&m| m.source.contains(&acc))
+                    .map(|m| m.get_destination(acc))
+                    .unwrap_or(acc)
+            })
+        })
+        .min()
+        .unwrap();
+
+    println!("-------------------- PART ONE -------------------------");
+    println!("Lowest location: {:?}", location);
+    println!("------------------ PART ONE (END) ---------------------");
+}
+
+fn part_two(_lines: &[String]) {
+    todo!();
 }
 
 fn main() {
     let lines = read_lines("input");
-    let (seeds, maps) = parse_lines(&lines);
 
-    println!("Seeds: {:?}", seeds);
-    println!("Maps: {:?}", maps);
+    part_one(&lines);
+    part_two(&lines);
 }
